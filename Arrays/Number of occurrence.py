fc98cd0ad5c9e5e79d0094a34c6d8709a690d0fa
AbdullahRFA <shakibrybmn@gmail.com>
2025-05-15 01:20:59 +0600

    added a problem that find the occurance of the target value

---
+++ Arrays/Number of occurrence.py
@@ -0,0 +1,39 @@
+"""
+Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[].
+
+Examples :
+
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+Output: 0
+Explanation: target = 4 is not present in the given array so the output is 0.
+Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+Output: 3
+Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+"""
+class Solution:
+    def countFreq(self, arr, target):
+        #code here
+        return arr.count(target)
+
+
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+import bisect
+#Main
+if __name__ == '__main__':
+    t = int(input())
+    while t:
+        t -= 1
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ans = ob.countFreq(A, D)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
