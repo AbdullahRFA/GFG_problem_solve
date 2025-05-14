88ee7ca9354f8b8362c549baaddc7213d9779ba7
AbdullahRFA <shakibrybmn@gmail.com>
2025-05-14 06:43:17 +0600

    added kadane's algorithm which find the contiguous subarray (containing at least one number) which has the largest sum, and return its sum

---
+++ Arrays/Kadane's Algorithm.py
@@ -0,0 +1,55 @@
+"""
+Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+Examples:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+Input: arr[] = [-2, -4]
+Output: -2
+Explanation: The subarray {-2} has the largest sum -2.
+Input: arr[] = [5, 4, 1, 7, 8]
+Output: 25
+Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+Constraints:
+1 ≤ arr.size() ≤ 105
+-109 ≤ arr[i] ≤ 104
+"""
+"""
+
+"""
+class Solution:
+    def maxSubArraySum(self, arr):
+        # Your code here
+        current_sum = max_sum = arr[0]
+        for x in range(1, len(arr)):
+            current_sum = max(arr[x], current_sum+arr[x])
+            max_sum = max(max_sum, current_sum)
+        return max_sum
+
+
+#{
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+
+        ob = Solution()
+
+        print(ob.maxSubArraySum(arr))
+        print("~")
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
